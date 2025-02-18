56aa75657feef977afde0c3291f8ee55cc74c573
Jiwoo So <58384366+jiwooproity@users.noreply.github.com>
2025-02-18 20:09:04 +0900

    Feat(today-button): add today button to calenar for set today ( reset )

--- src/features/calendar/ui/date-control/index.tsx
+++ src/features/calendar/ui/date-control/index.tsx
@@ -3,12 +3,17 @@ import module from "./date-control.module.css";
 import { Button } from "@/shared/components/button";
 import { DateControlProps } from "@/features/calendar/model/types";
 
-export function DateControl({ onPrev, date, onNext }: DateControlProps) {
+export function DateControl({ onPrev, date, onNext, getToday }: DateControlProps) {
   return (
     <div className={module.container}>
-      <Button className={`${module.arrow} ${module.left}`} onHandle={onPrev} text="" />
-      <h1 className={module.date}>{date}</h1>
-      <Button className={`${module.arrow} ${module.right}`} onHandle={onNext} text="" />
+      <div className={module.leftSide}>
+        <h1 className={module.date}>{date}</h1>
+      </div>
+      <div className={module.rightSide}>
+        <Button className={`${module.arrow} ${module.left}`} onHandle={onPrev} text="" />
+        <Button className={module.today} onHandle={getToday} text="오늘" />
+        <Button className={`${module.arrow} ${module.right}`} onHandle={onNext} text="" />
+      </div>
     </div>
   );
 }
